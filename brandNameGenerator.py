c90f76e341196e31192786d8dc25e3ecddf83ef3
smrhaque <97911494+smrhaque@users.noreply.github.com>
2022-09-14 12:49:22 -0230

    completed file

---
+++ brandNameGenerator.py
@@ -0,0 +1,8 @@
+# greeting for the user
+print("Welcome to Band Name Generator!\n")
+# the city that user grew up in
+city_user_grew_up_in = input("Where did you grow up?\n")
+# the name of user's pet
+name_of_user_pet = input("What is the name of your pet?\n")
+#printing the brand name
+print(f"Your Band Name is : {city_user_grew_up_in} {name_of_user_pet}")
