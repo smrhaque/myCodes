8c7a6ba6d7b09827053b9d0a2c3d031715a39847
smrhaque <97911494+smrhaque@users.noreply.github.com>
2022-09-14 21:36:26 -0230

    BMI calculator

--- BMI Calculator.py
+++ BMI Calculator.py
@@ -3,7 +3,7 @@ print("Welcome to BMI Calculator!")
 height = float(input("enter your height in m: "))
 weight = float(input("enter your weight in kg: "))
 # calculating BMI
-BMI = round((weight/(height**2)))
+BMI = round((weight / (height ** 2)))
 # variables to print
 U = "underweight"
 N = "normal weight"
@@ -12,12 +12,12 @@ O = "obese"
 CO = "clinically obese"
 # logic to determine BMI and giving an output
 if BMI < 18.5:
-  print(f"Your BMI is {BMI}, you are {U}.")
+    print(f"Your BMI is {BMI}, you are {U}.")
 elif 18.5 < BMI < 25:
-  print(f"Your BMI is {BMI}, you have a {N}.")
+    print(f"Your BMI is {BMI}, you have a {N}.")
 elif 25 < BMI < 30:
-  print(f"Your BMI is {BMI}, you are {SO}.")
+    print(f"Your BMI is {BMI}, you are {SO}.")
 elif 30 < BMI < 35:
-  print(f"Your BMI is {BMI}, you are {O}.")
+    print(f"Your BMI is {BMI}, you are {O}.")
 else:
-  print(f"Your BMI is {BMI}, you are {CO}.")
+    print(f"Your BMI is {BMI}, you are {CO}.")
